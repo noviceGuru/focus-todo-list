708a64b29c3fb8fa55e5d0b84ed0164c240acfce
noviceGuru <amin.arabi@gmail.com>
2024-01-26 08:39:10 +0100

    ovelay developed will open and close on click on an envelope sign in the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import BackTable from "./components/molecules/back-table/back-table"
+import { useState } from "react"
+
+import MainTodo from "components/atoms/main-todo/main-todo"
+import Overlay from "components/atoms/overlay/overlay"
+import BackTable from "components/molecules/back-table/back-table"
+
+import OpenEnvelope from "assets/open-envelope.svg"
+import ClosedEnveloper from "assets/closed-envelope.svg"
 
 function App() {
+    const [show, setShow] = useState<boolean>(false)
+
     const todos = [
         {
             id: "1",
@@ -16,8 +25,16 @@ function App() {
         },
     ]
     return (
-        <div className="flex justify-center items-center h-full">
-            <BackTable todos={todos} />
+        <div className="flex flex-col justify-center items-center h-full">
+            <MainTodo task={"The very main task"} />
+            <Overlay show={show} setShow={setShow}>
+                <BackTable todos={todos} />
+            </Overlay>
+            <img
+                src={show ? OpenEnvelope : ClosedEnveloper}
+                onClick={() => setShow(true)}
+                className="absolute h-12 bottom-14 md:bottom-36 cursor-pointer"
+            />
         </div>
     )
 }
--- src/components/atoms/main-todo/main-todo.tsx
+++ src/components/atoms/main-todo/main-todo.tsx
@@ -1,12 +1,12 @@
-import CircleIcon from "../../../assets/circle.svg"
-import CircleCheckIcon from "../../../assets/circle-check.svg"
+import CircleIcon from "assets/circle.svg"
+import CircleCheckIcon from "assets/circle-check.svg"
 
 export default function MainTodo({ task }: { task: string }) {
     return (
         <div className="flex gap-8">
-            <div className="bg-slate-400 cursor-pointer">
+            <div className="cursor-pointer">
                 <img src={CircleIcon} className="h-6 absolute" />
-                <img src={CircleCheckIcon} className="h-6 absolute opacity-0 hover:opacity-100 transition-opacity" />
+                <img src={CircleCheckIcon} className="h-6 absolute opacity-0 hover:opacity-100 transition-opacity active:opacity-40" />
             </div>
             <span>{task}</span>
         </div>
