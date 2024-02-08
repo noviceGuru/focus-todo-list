4b453db1d21d01f177365d4b870838448bc644a8
noviceGuru <amin.arabi@gmail.com>
2024-02-08 02:40:00 +0100

    clear all button added

--- src/components/molecules/back-table/back-table.tsx
+++ src/components/molecules/back-table/back-table.tsx
@@ -6,6 +6,7 @@ import AllDoneNote from "components/atoms/all-done-note/all-done-note"
 import TodoRow from "components/atoms/todo-row/todo-row"
 
 import AddSquareIcon from "assets/add-square.svg"
+import ClearAll from "assets/clear-all.svg"
 
 export default function ({
     todos,
@@ -26,8 +27,12 @@ export default function ({
         setEditingKey(newId)
     }
 
+    const deleteAllTasks = () => {
+        setTodos([])
+    }
+
     const containersClasses = "w-3/4 flex flex-col gap-4 border-2 p-8 rounded-lg bg-slate-400"
-    
+
     if (todos.length === 0) {
         return (
             <div className={containersClasses}>
@@ -41,27 +46,35 @@ export default function ({
 
     return (
         <div className={containersClasses}>
-            <button className="self-end" onClick={addTodoInput}>
-                <img src={AddSquareIcon} className="h-8 hover:brightness-200 transition-all duration-300" />
-            </button>
+            <div className="flex justify-between">
+                <button className="self-end" onClick={deleteAllTasks}>
+                    <img
+                        src={ClearAll}
+                        className="h-8 hover:brightness-200 transition-all duration-300"
+                        title="Clear All Tasks"
+                    />
+                </button>
+                <button className="self-start" onClick={addTodoInput}>
+                    <img
+                        src={AddSquareIcon}
+                        className="h-8 hover:brightness-200 transition-all duration-300"
+                        title="Add New Task"
+                    />
+                </button>
+            </div>
             <div className="rounded-xl overflow-hidden table-auto">
-                    <ReactSortable
-                        list={todos}
-                        setList={setTodos}
-                        easing="ease-out"
-                        animation={200}
-                    >
-                        {todos.map(({ id, task }) => (
-                            <TodoRow
-                                id={id}
-                                task={task}
-                                setTodos={setTodos}
-                                key={id}
-                                setEditingKey={setEditingKey}
-                                isEditing={editingKey === id}
-                            />
-                        ))}
-                    </ReactSortable>
+                <ReactSortable list={todos} setList={setTodos} easing="ease-out" animation={200}>
+                    {todos.map(({ id, task }) => (
+                        <TodoRow
+                            id={id}
+                            task={task}
+                            setTodos={setTodos}
+                            key={id}
+                            setEditingKey={setEditingKey}
+                            isEditing={editingKey === id}
+                        />
+                    ))}
+                </ReactSortable>
             </div>
         </div>
     )
